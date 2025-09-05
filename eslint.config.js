8b0d070cf714f2eb558ca84a1d33b00617bf29dd
garfnadeck <serraopinto@gmail.com>
2025-09-05 14:32:27 -0300

    feat(all): movie app
    finishing upload

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,21 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const tseslint = require("typescript-eslint");
+const prettier = require("eslint-config-prettier");
+const eslintPluginPrettier = require("eslint-plugin-prettier");
 
 module.exports = defineConfig([
   expoConfig,
+  ...tseslint.configs.recommended,
+  prettier,
   {
-    ignores: ['dist/*'],
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+    ignores: ["dist/*"],
   },
 ]);
---
+++ metro.config.js
@@ -0,0 +1,11 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  assert: require.resolve("assert/"),
+};
+
+module.exports = withNativeWind(config, { input: "./app/globals.css" });
